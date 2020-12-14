9d12803bae1cba6d2a21904a0609cfa848cef4cb
Glodeanu Theodor <theodor.glodeanu@gmail.com>
2020-12-14 16:57:43 +0200

    Added the structure for the route

--- server/routers/index.js
+++ server/routers/index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const router = express.Router()
+<<<<<<< HEAD
 const deliverableRouter = require('./deliverables')
 const teamsRouter = require('./teams')
 
 router.use('/deliverable', deliverableRouter)
+=======
+const teamsRouter = require('./teams')
+
+>>>>>>> Added the structure for the route
 router.use('/teams', teamsRouter)
 
 module.exports = router;
