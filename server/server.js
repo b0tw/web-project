427219e089a2727a5a3efc9a6bd61e084568b267
Glodeanu Theodor <theodor.glodeanu@gmail.com>
2020-12-14 16:18:07 +0200

    Removed unnecessary package

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const fs = require('fs');
 const settings = JSON.parse(fs.readFileSync('appsettings.json'));
 const logger = require('morgan');
-const bodyParser = require('body-parser')
 const router = require('./routers')
 
 const app = express();
@@ -10,7 +9,7 @@ const app = express();
 // Setup logger.
 app.use(logger('dev'));
 
-app.use(bodyParser.json())
+app.use(express.json())
 app.use('/api', router)
 
 app.listen(settings.port, () => console.log(`Listening on port ${settings.port}...`));
