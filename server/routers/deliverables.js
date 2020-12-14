c91627c55e920d6aa6b18e4d456287835345ed5d
Glodeanu Theodor <theodor.glodeanu@gmail.com>
2020-12-14 15:37:15 +0200

    Added route for get and update deliverable

--- server/routers/deliverables.js
+++ server/routers/deliverables.js
@@ -13,7 +13,7 @@ router.post('/add', (req, res) => {
     else if (description.length > 200) {
         res.status(400).send({ "message": "Description is too large" })
     }
-    
+
     //creating the object
     let deliverable = {
         projectId,
@@ -24,8 +24,36 @@ router.post('/add', (req, res) => {
     //storing it into the database
 
 
-    //response
+    //response (will be a message after the db is made)
     res.status(200).send(deliverable)
 })
 
+router.get('/get', (req, res) => {
+    projectId = req.body.projectId
+
+    //search in db
+
+
+    res.status(200).send(projectId)
+})
+
+router.put('/update', (req, res) => {
+    projectId = req.body.projectId
+    title = req.body.title
+    description = req.body.description
+
+    //validate projectId
+
+
+    if (title !== null && title.length < 10) {
+        //update title in the db
+
+    }
+
+    if(description !== null && description.length <20){
+        //update description in the db
+
+    }
+    res.status(400).send({"message":"db"})
+})
 module.exports = router
