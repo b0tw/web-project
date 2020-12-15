759156bded7ca0ccee3cbb26f8dbaf193a15d58e
Glodeanu Theodor <theodor.glodeanu@gmail.com>
2020-12-15 19:47:22 +0200

    Added team functionality

--- server/routers/index.js
+++ server/routers/index.js
@@ -1,22 +1,9 @@
 const express = require('express')
 const router = express.Router()
-<<<<<<< HEAD
-<<<<<<< HEAD
 const deliverableRouter = require('./deliverables')
 const teamsRouter = require('./teams')
 
 router.use('/deliverable', deliverableRouter)
-<<<<<<< HEAD
-=======
-const teamsRouter = require('./teams')
-
->>>>>>> Added the structure for the route
-=======
->>>>>>> Added the structure for the route
-=======
-const teamsRouter = require('./teams')
-
->>>>>>> Added the structure for the route
 router.use('/teams', teamsRouter)
 
 module.exports = router;
