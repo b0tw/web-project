83dd490f7e7c21baf0a2d29930a54f890c6006f2
Glodeanu Theodor <theodor.glodeanu@gmail.com>
2020-12-15 19:47:22 +0200

    Added team functionality

--- server/server.js
+++ server/server.js
@@ -2,39 +2,17 @@ const express = require('express');
 const fs = require('fs');
 const settings = JSON.parse(fs.readFileSync('appsettings.json'));
 const logger = require('morgan');
-<<<<<<< HEAD
-<<<<<<< HEAD
 const context = require('./entities/database/context');
-<<<<<<< HEAD
-=======
->>>>>>> Added the structure for the route
-=======
->>>>>>> Added the structure for the route
-=======
->>>>>>> Added the structure for the route
 const router = require('./routers')
 
 const app = express();
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> Added the structure for the route
 context.connection.authenticate().then(() => {
   console.log('Successfully connected to the database...');
   context.connection.sync();
   console.log('Database is up to date.');
   // Setup logger.
   app.use(logger('dev'));
-=======
-// Setup logger.
-app.use(logger('dev'));
-app.use(express.json());
-app.use('/api', router)
-<<<<<<< HEAD
->>>>>>> Added the structure for the route
-=======
->>>>>>> Added the structure for the route
 
   app.use(express.json())
   app.use('/api', router)
