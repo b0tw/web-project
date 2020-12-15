868dc61f58388046bb0a1443676ecfebfdb47a54
Glodeanu Theodor <theodor.glodeanu@gmail.com>
2020-12-15 19:38:43 +0200

    Finished team management feature

--- server/routers/teams.js
+++ server/routers/teams.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router();
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> Finished team management feature
 const Team = require('../entities/database/context').Team
 
 router.get('/get', async (req, res) => {
@@ -18,6 +21,7 @@ router.get('/get', async (req, res) => {
     else {
         res.status(200).send({ team })
     }
+<<<<<<< HEAD
 
 })
 
@@ -51,18 +55,46 @@ router.delete('/delete', async (req, res) => {
     }
 
 =======
+=======
+>>>>>>> Finished team management feature
 
-router.get('/get',(req,res)=>{
-    res.status(400)
 })
 
-router.post('/add',(req,res)=>{
-    res.status(400)
+router.post('/add', async (req, res) => {
+    const name = req.body.name
+
+    const existantTeam = await Team.findOne({ where: { name } })
+    if (existantTeam !== null) {
+        res.status(400).send({ "message": "The team name is already taken" })
+    }
+    else {
+        try {
+            const team = await Team.create({ name })
+            res.status(200).send({ "message": "Team " + team.name + " was created." })
+        }
+        catch (err) {
+            res.status(400).send({ "message": "something bad happened" })
+        }
+    }
 })
 
+<<<<<<< HEAD
 router.put('/update',(req,res)=>{
     res.status(400)
 >>>>>>> Added the structure for the route
+=======
+router.delete('/delete', async (req, res) => {
+    const name = req.body.name
+    try {
+        const team = await Team.findOne({ where: { name } })
+        await team.destroy()
+        res.status(200).send({ "message": "Team was deleted" })
+    }
+    catch (err) {
+        res.status(400).send({ "message": "Something happened at deletion" })
+    }
+
+>>>>>>> Finished team management feature
 })
 
 module.exports = router
