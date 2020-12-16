f8efb770129fd5d01551dd5eb411ece1777b41d1
Glodeanu Theodor <theodor.glodeanu@gmail.com>
2020-12-16 15:33:53 +0200

    Fix

--- server/entities/database/team.js
+++ server/entities/database/team.js
@@ -6,6 +6,11 @@ const dataTypes = sequelize.DataTypes;
 class Team extends Model {}
 
 Team.init({
+  id: {
+    type: dataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true
+  },
   name: {
     type: dataTypes.STRING(45),
     allowNull: false
