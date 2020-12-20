14c3bd345d909b78d6f81f9dfd6a0b10f4e7d12b
Filip Dutescu <filip.dutescu@gmail.com>
2020-12-20 13:15:53 +0200

    add deliverables to team router

--- server/routers/teams.js
+++ server/routers/teams.js
@@ -10,16 +10,9 @@ const { Op } = require("sequelize");
 router.use(authentication());
 
 router.get('/', async (req, res) => {
-
   // get the all teams
-  try {
-    const allTeams = await Team.findAll()
-    return res.status(200).send(allTeams)
-  }
-  catch (err) {
-    return res.status(400).send({ "message": "Something bad happened" })
-  }
-
+  const allTeams = await Team.findAll()
+  return res.status(200).send(allTeams)
 });
 
 router.get('/:id', async (req, res) => {
@@ -31,7 +24,7 @@ router.get('/:id', async (req, res) => {
     return res.status(400).send(apiError.InvalidRequest);
   }
 
-  let team = await context.Team.findOne({ where: { id: id }, include: [{ model: context.User, attributes: ['id', 'surname', 'name'] }] });
+  let team = await context.Team.findOne({ where: { id: id }, include: [{ model: context.User, attributes: ['id', 'surname', 'name'] }, { model: context.Deliverable }] });
   if (team == null) {
     return res.status(400).send(apiError.InvalidRequest);
   }
@@ -49,7 +42,7 @@ router.post('/', async (req, res) => {
   //search in the db the user
   const user = await User.findOne({ where: username })
   //if the user is a professor, the addition can happen
-  if (user.is_professor === true) {
+  if (user && user.is_professor == 1) {
     const existantTeam = await Team.findOne({
       where: {
         [Op.or]: [name, project_name]
@@ -59,17 +52,12 @@ router.post('/', async (req, res) => {
       return res.status(400).send({ "message": "The team name or the project is already taken" })
     }
     else {
-      try {
-        const team = await Team.create({ name, project_name })
-        return res.status(200).send({ "message": "Team " + team.name + " was created." })
-      }
-      catch (err) {
-        return res.status(400).send({ "message": "something bad happened" })
-      }
+      const team = await Team.create({ name, project_name })
+      return res.status(200).send({ "message": "Team " + team.name + " was created." })
     }
   }
   else {
-    return res.status(400).send({ "message": apiError.Unauthorized })
+    return res.status(401).send(apiError.Unauthorized);
   }
 
 });
@@ -82,21 +70,20 @@ router.delete('/:id', async (req, res) => {
   const user = await User.findOne({ where: username })
 
   //if the user is a professor, the deletion can happen
-  if (user.is_professor === true) {
+  if (user && user.is_professor == 1) {
     const id = req.params.id
-    try {
-      const team = await Team.findOne({ where: { id }, include: [context.User, context.Project] })
-      team.removeProjects(team.Projects);
-      team.removeUsers(team.Users);
-      await team.destroy()
-      return res.status(200).send({ "message": "Team was deleted" })
-    }
-    catch (err) {
-      return res.status(400).send({ "message": "Something happened at deletion" })
-    }
+    const team = await Team.findOne({ where: { id }, include: [context.User, context.Deliverables, context.Jury ] })
+
+    team.removeProjects(team.Projects);
+    team.removeUsers(team.Users);
+    team.removeDeliverables(team.Deliverables);
+    team.removeDeliverables(team.Jury);
+    await team.destroy();
+
+    return res.status(200).send({ "message": "Team was deleted" })
   }
   else {
-    return res.status(400).send({ "message": apiError.Unauthorized })
+    return res.status(401).send(apiError.Unauthorized);
   }
 });
 
@@ -157,6 +144,88 @@ router.delete('/:id/members', async (req, res) => {
   await team.save();
 
   return res.status(200).json({ message: 'Team members updated successfully.' });
+});
+
+router.post('/:id/deliverables', async (req, res) => {
+    let id = parseInt(req.params.id),
+      title = req.body.title,
+      description = req.body.description,
+      url = req.body.url,
+      team_id = req.body.team_id;
+
+    //validating the received data
+    if (isNaN(id) || title === null || title.length > 20
+      || description == null || description.length > 200
+      || url === null) {
+        return res.status(400).send(apiError.InvalidRequest);
+    }
+
+    const team = await context.Team.findOne({ where: { id: team_id } })
+    if (team == null) {
+        return res.status(400).send(apiError.InvalidRequest)
+    }
+    const del = await context.Deliverable.create({ title, description, url, team_id })
+
+    team.Deliverables.push(del);
+    team.setDeliverables(team.Deliverables);
+    await team.save()
+
+    return res.status(200).send({ "message": del.title + " was created." })
+});
+
+router.put('/:id/deliverables/:deliverableId', async (req, res) => {
+    let id = parseInt(req.params.id),
+      deliverableId = parseInt(req.params.deliverableId),
+      title = req.body.title,
+      description = req.body.description,
+      link = req.body.link;
+
+    let deliverable = await context.Deliverable.findOne({ where: { id: deliverableId, team_id: id } })
+    if (deliverable == null) {
+        return res.status(400).send(apiError.InvalidRequest);
+    }
+
+    if (title !== null && title.length < 20) {
+        deliverable.title = title
+    }
+    if (description !== null && description.length < 200) {
+        deliverable.description = description
+    }
+    if (link !== null) {
+        deliverable.link = link
+    }
+
+    await deliverable.save()
+    return res.status(200).send({ "message": "Changes were made successfully" })
+});
+
+router.delete('/:id/deliverables', async (req, res) => {
+    let id = parseInt(req.params.id),
+      deliverables = req.body; 
+
+    //validating the received data
+    if (isNaN(id) || typeof deliverables != 'object' || deliverables.some(d => typeof d.id == 'number')) {
+        return res.status(400).send(apiError.InvalidRequest);
+    }
+
+    const team = await context.Team.findOne({ where: { id: id } })
+    if (team == null) {
+        return res.status(400).send(apiError.InvalidRequest)
+    }
+    
+    const newDeliverables = [];
+    team.Deliverables.forEach(d =>
+    {
+      if(!deliverables.some(nd => nd.id == d.id))
+      {
+        newDeliverables.push(d);
+      }
+    });
+
+    team.setDeliverables(newDeliverables);
+    await team.save()
+
+    return res.status(200).send({ "message": "Team deliverables updated successfully." })
 });
 
 module.exports = router
