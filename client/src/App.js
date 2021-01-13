8b9ac79d166d324556795889966db9d4de72d711
Filip Dutescu <filip.dutescu@gmail.com>
2021-01-13 23:55:07 +0200

    add navbar and router

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,71 @@
 import logo from './logo.svg';
 import './App.css';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import {
+  BrowserRouter as Router,
+  Link,
+  Switch,
+  Redirect,
+  Route,
+} from 'react-router-dom';
+import {
+  Collapse,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+  Nav,
+  NavItem,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  UncontrolledDropdown,
+} from 'reactstrap';
+import { useState } from 'react';
 
 function App() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  function validateEmail()
-  {
-    
-  }
-
-  function onSubmit(ev)
-  {
-    ev.preventDefault();
-  }
+  const [isCollapsed, toggleCollapse] = useState(true);
+  const [token, setAuthToken] = useState('');
+  const toggle = () => toggleCollapse(!isCollapsed);
 
   return (
-    <div className="App">
-      <Form onSubmit={onSubmit}>
-      <Form.Group size="lg" controlId="email">
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            autoFocus
-            type="email"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group size="lg" controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-          />
-        </Form.Group>
-        <Button block size="lg" type="submit" disabled={!validateForm()}>
-          Login
-        </Button>
-      </Form>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar color="dark" dark expand="md">
+          <NavbarBrand href="/" className="mr-auto">AnonGrading</NavbarBrand>
+          <NavbarToggler onClick={toggle} className="mr-2" />
+          <Collapse isOpen={!isCollapsed} navbar>
+          { token.length > 0
+          ? <Nav navbar className="mr-auto">
+              <NavItem className="mx-2">
+                <Link to="/">Home</Link>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar className="mx-2">
+                <DropdownToggle nav caret>Grading</DropdownToggle>
+                <DropdownMenu right className="bg-secondary">
+                  <DropdownItem tag={Link} to="/students">Students</DropdownItem>
+                  <DropdownItem tag={Link} to="/teams">Teams</DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem tag={Link} to="/grade">Grade</DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+              <NavItem className="mx-2">
+                <Link to="/professors">Professors</Link>
+              </NavItem>
+            </Nav>
+          : <Nav navbar className="mr-auto">
+              <NavItem className="mx-2">
+                <Link to="/sign-up">Sign up</Link>
+              </NavItem>
+              <NavItem className="mx-2">
+                <Link to="/login">Login</Link>
+              </NavItem>
+            </Nav>}
+          </Collapse>
+        </Navbar>
+
+        <Switch>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
